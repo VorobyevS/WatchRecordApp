137205130e547b98ac7e9451bcd89a869f9fb62d
ios dev <vorobyev.semyon@gmail.com>
2018-11-19 16:51:47 +0300

    WatchKit core  done

--- WatchRecordApp WatchKit Extension/InterfaceController.swift
+++ WatchRecordApp WatchKit Extension/InterfaceController.swift
@@ -8,10 +8,20 @@
 
 import WatchKit
 import Foundation
+import AVFoundation
+import WatchConnectivity
 
 
-class InterfaceController: WKInterfaceController {
-
+final class InterfaceController: WKInterfaceController {
+    private var isRecording = false
+    private var recorder:AVAudioRecorder!
+    private var recordURL: URL!
+    let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    
+    //MARK: Outlets
+    @IBOutlet private weak var image: WKInterfaceImage!
+    
+    //MARK: Lifecycle
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
@@ -19,13 +29,90 @@ class InterfaceController: WKInterfaceController {
     }
     
     override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        if (WCSession.isSupported()) {
+            let session = WCSession.default
+            session.delegate = self
+            session.activate()
+        }
     }
     
     override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+        stopRecording()
     }
+    
+    //MARK: Actions
+    @IBAction private func startStopButtonClicked() {
+        isRecording ? stopRecording() : startRecording()
+    }
+    
+    private func startRecording() {
+        guard recorder == nil else { return }
+        recordURL = path.appendingPathComponent(getTitleString())
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue]
+        do {
+            recorder = try AVAudioRecorder.init(url: recordURL, settings: settings)
+            recorder.delegate = self
+            recorder.record()
+            setRecordImage(play: true)
+        }
+        catch {
+            print(error)
+        }
+    }
+    
+    private func stopRecording() {
+        guard recorder != nil else { return }
+        recorder.stop()
+        recorder = nil
+        setRecordImage(play: false)
+    }
+    
+    private func sendFile() {
+        guard let record = recordURL else { return }
+        WCSession.default.transferFile(record, metadata: nil)
+    }
+    
+    private func getTitleString() -> String {
+        let date = Date()
+        let formater = DateFormatter()
+        formater.dateFormat = "dd.MM.yyyy.mm.ss"
+        return "record" + formater.string(from: date) + ".m4a"
+    }
+    
+    private func setRecordImage(play: Bool) {
+        play ? image.setImageNamed("stop") : image.setImageNamed("play")
+        isRecording = play
+    }
+    
+}
 
+//MARK: WCSessionDeelgate
+extension InterfaceController : WCSessionDelegate {
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        
+    }
+    
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+        guard let fileName = message["file"] as? String else { return }
+        DispatchQueue.main.async {[weak self] in
+            self?.presentAlert(withTitle: "File transfered",
+                               message: fileName,
+                               preferredStyle: .alert,
+                               actions: [WKAlertAction(title: "Ok", style: .cancel, handler: {})])
+        }
+        try? FileManager.default.removeItem(at: path.appendingPathComponent(fileName))
+    }
+}
+
+//MARK: AVAudioRecorderDelegate
+extension InterfaceController: AVAudioRecorderDelegate {
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if flag {
+            sendFile()
+        }
+    }
 }
--- WatchRecordApp/ViewController.swift
+++ WatchRecordApp/ViewController.swift
@@ -7,14 +7,106 @@
 //
 
 import UIKit
+import AVFoundation
+import WatchConnectivity
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    private var items = [AVURLAsset]()
+    private let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    private var player : AVAudioPlayer?
+    
+    //MARK: Outlets
+    @IBOutlet private weak var recordsTableView: UITableView!
+    
+    //MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        commonInit()
+        recordsTableView.register(UINib(nibName: "RecordTableViewCell", bundle: nil), forCellReuseIdentifier: RecordTableViewCell.indetifier)
+        if (WCSession.isSupported()) {
+            let session = WCSession.default
+            session.delegate = self
+            session.activate()
+        }
     }
-
-
+    
+    private func commonInit() {
+        guard let tracks = try? FileManager.default.contentsOfDirectory(atPath: dir.path) else { return }
+        for item in tracks where item.hasSuffix(".m4a") {
+            items.append(AVURLAsset.init(url: URL.init(fileURLWithPath: item)))
+        }
+    }
+    
+    private func getFileNameFrom(URL: URL)-> String {
+        return String(URL.path.split(separator: "/").last!)
+    }
+    
 }
 
+//MARK: TableViewDataSource, TableViewDelegate
+extension ViewController : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = recordsTableView.dequeueReusableCell(withIdentifier: RecordTableViewCell.indetifier) as! RecordTableViewCell
+        cell.nameLabel.text = FileManager.default.displayName(atPath: items[indexPath.row].url.path)
+        cell.durationLabel.text = String(items[indexPath.row].duration.seconds)
+        cell.deleteAction = {[weak self] in
+            guard let `self` = self else {return}
+            do{
+                let fileName = self.getFileNameFrom(URL: self.items[indexPath.row].url)
+                try FileManager.default.removeItem(at: self.dir.appendingPathComponent(fileName))
+                self.items.remove(at: indexPath.row)
+                self.recordsTableView.reloadData()
+            }
+            catch {
+                print(error)
+            }
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let fileName = self.getFileNameFrom(URL: self.items[indexPath.row].url)
+        let fileUrl = dir.appendingPathComponent(fileName)
+        player = try? AVAudioPlayer.init(contentsOf: fileUrl)
+        player?.play()
+    }
+}
+
+//MARK  WCSessionDelegate
+extension ViewController : WCSessionDelegate {
+    func sessionDidBecomeInactive(_ session: WCSession) {
+        
+    }
+    
+    func sessionDidDeactivate(_ session: WCSession) {
+        
+    }
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        
+    }
+    
+    func session(_ session: WCSession, didReceive file: WCSessionFile) {
+        do {
+            let fileName = self.getFileNameFrom(URL: file.fileURL)
+            let fileURL = dir.appendingPathComponent(fileName)
+            try FileManager.default.moveItem(atPath: file.fileURL.path, toPath: fileURL.path)
+            items.append(AVURLAsset.init(url: fileURL))
+            DispatchQueue.main.async { [weak self] in
+                self?.recordsTableView.reloadData()
+            }
+            WCSession.default.sendMessage(["file": fileName],
+                                          replyHandler: nil,
+                                          errorHandler: nil)
+        }
+        catch {
+            print(error)
+        }
+    }
+    
+}
